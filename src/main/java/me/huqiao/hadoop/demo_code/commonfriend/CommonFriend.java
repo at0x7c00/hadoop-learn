ef7baf27220a4a021e6974b3d610b4b6ebef814d
xooxle <zoozle@qq.com>
2017-12-22 19:37:46 +0800

    common friend

---
+++ src/main/java/me/huqiao/hadoop/demo_code/commonfriend/CommonFriend.java
@@ -0,0 +1,22 @@
+package me.huqiao.hadoop.demo_code.commonfriend;
+
+/**
+ * 现有每个人的好友列表如下：
+ * <pre>
+ * A: B,F,G
+ * B: C,E
+ * D: B,T,G
+ * C: E,F
+ * </pre>
+ * 需要得到如下结果：
+ * <pre>
+ * A-C: F
+ * A-D: B,G
+ * </pre>
+ * @author huqiao
+ */
+public class CommonFriend {
+
+	
+	
+}
